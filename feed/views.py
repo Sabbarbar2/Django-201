482c4598697cba32f47dc0ca27419e0c27aececd
Constantin Sabin <sabinconstantin60@yahoo.com>
2023-04-03 12:07:52 +0300

    added templates and fixed urls

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+#from django.db.models.query import QuerySet
 
 # Create your views here.
+
+class HomePage(ListView):
+    http_method_names=["get"]
+    template_name = "feed\homepage.html"
+    model = Post
+    context_object_name = "posts"
+    queryset = Post.objects.all().order_by('-id')[0:30]
+
