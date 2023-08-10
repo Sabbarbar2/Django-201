a2efcd0e7804eb7c79ce52d49163feff273594b1
Constantin Sabin <sabinconstantin60@yahoo.com>
2023-08-10 23:45:29 +0300

    added follow actions to the button

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseBadRequest, JsonResponse
 
 from feed.models import Post
 from followers.models import Follower
@@ -18,3 +20,41 @@ class ProfileDetailView(DetailView):
         context['total_posts'] = Post.objects.filter(author=user).count()
         context['total_followers'] = Follower.objects.filter(followed_by=user).count()
         return context
+    
+class FollowView(LoginRequiredMixin, View):
+    http_method_names = ["post"]
+    
+    def post(self, request, *args, **kwargs):
+        data = request.POST.dict()
+        
+        if "action" not in data or "username" not in data:
+            return HttpResponseBadRequest("Missing data")
+        
+        try:
+            other_user = User.objects.get(username=data["username"])
+        except User.DoesNotExist:
+            return HttpResponseBadRequest("Invalid username")
+        
+        if data['action'] == 'follow':
+            # Follow the user
+            follower, created = Follower.objects.get_or_create(
+                followed_by=request.user,
+                following=other_user
+            )
+        else:
+            # Unfollow the user
+            try:
+                follower = Follower.objects.get(
+                    followed_by=request.user,
+                    following=other_user
+                )
+            except Follower.DoesNotExist:
+                follower = None
+                
+            if follower:
+                follower.delete()
+            
+        return JsonResponse({
+        'success': True,
+        'wording': "Unfollow" if data['action'] == 'follow' else "Follow",
+        })
